f3dd083bada9374737ada2fe45bbd4bafefbdae5
Sleepingmag <100070910+sleepingmag@users.noreply.github.com>
2024-02-19 18:46:48 +0200

    add function buttons

--- protocol.js
+++ protocol.js
@@ -1,3 +1,15 @@
 function faq (){
-window.location.href = "/pages/faq.html";
+window.location.href = "pages/faq.html";
+}
+function rules(){
+window.location.href = "pages/rules.html"
+}
+function news(){
+window.location.href = "pages/news.html"
+}
+function ds(){
+window.open("https://discord.gg/ECztC7zpe9", "_blank");
+}
+function vk(){
+window.open("https://vk.com/club224745781","_blank")
 }

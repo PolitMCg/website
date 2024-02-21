4b247a70f703f0afb9563880180680a0f1b8b716
Sleepingmag <100070910+sleepingmag@users.noreply.github.com>
2024-02-21 21:35:42 +0200

    fix bug

--- protocol.js
+++ protocol.js
@@ -1,14 +1,14 @@
 function home(){
-window.location.href = "index.html";
+window.location.href = "/website.main/index.html";
 }
 function faq(){
-window.location.href = "pages/faq.html";
+window.location.href = "/website.main/pages/faq.html";
 }
 function rules(){
-window.location.href = "pages/rules.html"
+window.location.href = "/website.main/pages/rules.html"
 }
 function news(){
-window.location.href = "pages/news.html"
+window.location.href = "/website.main/pages/news.html"
 }
 function ds(){
 window.open("https://discord.gg/ECztC7zpe9", "_blank");

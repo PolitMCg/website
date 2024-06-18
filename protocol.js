453938b36c62194b5f5fb3b1299a83de9d92caf5
Sleepingmag <100070910+sleepingmag@users.noreply.github.com>
2024-06-18 21:22:09 +0300

    откат

--- protocol.js
+++
@@ -1,9 +0,0 @@
-// scripts/menu.js
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const mobileMenu = document.getElementById('mobile-menu');
-
-    hamburgerMenu.addEventListener('click', function() {
-        mobileMenu.classList.toggle('show');
-    });
-});
